49fd262bacff1965f77640ebfeccd1eca486f1d5
mixaron <tosha.ryabchuk@gmail.com>
2025-12-11 16:04:32 +0400

    add: KafkaConfig.java with CommonErrorHandler to notification and audit services

---
+++ notification-service/src/main/java/ru/mixaron/notificationservice/config/KafkaConfig.java
@@ -0,0 +1,19 @@
+package ru.mixaron.notificationservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public CommonErrorHandler errorHandler(KafkaTemplate<Object, Object> template) {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(template);
+        return new DefaultErrorHandler(recoverer, new FixedBackOff(0L, 0));
+    }
+}
