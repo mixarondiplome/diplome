7f5c25804b0d0d75e2c82d3c997df970b2b98465
mixaron <tosha.ryabchuk@gmail.com>
2025-12-15 17:05:04 +0400

    refactor: replace sout to logger

--- transaction-service/src/main/java/ru/mixaron/transactionservice/service/TransactionProducer.java
+++ transaction-service/src/main/java/ru/mixaron/transactionservice/service/TransactionProducer.java
@@ -1,6 +1,8 @@
 package ru.mixaron.transactionservice.service;
 
 import com.example.TransactionEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -9,6 +11,9 @@ import java.time.Instant;
 
 @Service
 public class TransactionProducer {
+
+    private final static Logger logger = LoggerFactory.getLogger(TransactionProducer.class);
+
     private final KafkaTemplate<String, TransactionEvent> kafkaTemplate;
     private long count = 1L;
     public TransactionProducer(KafkaTemplate<String, TransactionEvent> kafkaTemplate) {
@@ -24,6 +29,6 @@ public class TransactionProducer {
                 .setTimestamp(Instant.now())
                 .build();
         kafkaTemplate.send("transaction-events", Long.toString(event.getId()), event);
-        System.out.println("Sent transaction event: " + event);
+        logger.info("Sent transaction event: {}", event);
     }
 }
